aa71e495f81ad0faa65c831f197f018c72cf66b6
Claudio Mallea <claudiomallea.a@gmail.com>
2023-02-03 01:22:37 -0300

    adding friends and avatars

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+// console.log(friends);
+
+// function onHoverFriend(e){
+//     console.log(e);
+//     e.target.firstElementChild.style.display="inline";
+
+// }
+
+// for (friend of friends){
+//     friend.addEventListener('mouseover', onHoverFriend)
+    
+
+// }
